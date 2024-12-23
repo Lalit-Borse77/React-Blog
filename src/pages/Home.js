5c07dbcb044bbbf63c767211a4ff2ebd2580e8aa
Lalitanilborse <borselalit66@gmail.com>
2024-12-23 16:57:11 +0530

    login error fixed

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='mb-20'>
+            <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>Hello, welcome to my blog</h1>
+            <p className='mx-auto leading-relaxed text-base mb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe quae fuga nostrum tenetur repellat quidem, aliquid ea facilis vitae totam rerum! Beatae modi tempore nemo rerum dolorum molestias aliquid blanditiis. Explicabo rem est ex mollitia accusamus, sed repellat, error illo modi, tempore quam deleniti labore vel quis neque laudantium maxime.</p>
+            <p className='mx-auto leading-relaxed text-base mb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe quae fuga nostrum tenetur repellat quidem, aliquid ea facilis vitae totam rerum! Beatae modi tempore nemo rerum dolorum molestias aliquid blanditiis. Explicabo rem est ex mollitia accusamus, sed repellat, error illo modi, tempore quam deleniti labore vel quis neque laudantium maxime.</p>
+            <p className='mx-auto leading-relaxed text-base mb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe quae fuga nostrum tenetur repellat quidem, aliquid ea facilis vitae totam rerum! Beatae modi tempore nemo rerum dolorum molestias aliquid blanditiis. Explicabo rem est ex mollitia accusamus, sed repellat, error illo modi, tempore quam deleniti labore vel quis neque laudantium maxime.</p>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1 className='sm:text-4xl text-2xl font0-bold my-6 text-gray-900'>404: Page not found</h1>
+        </div>
+    )
+}
+
+export default NotFound
